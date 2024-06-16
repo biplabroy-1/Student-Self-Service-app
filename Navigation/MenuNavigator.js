8acecd71344a84975709916dd3db7b80a0e95dc1
Biplab Roy <biplabr119201@gmail.com>
2024-06-17 05:16:31 +0530

    all done

--- Navigation/MenuNavigator.js
+++ Navigation/MenuNavigator.js
@@ -3,8 +3,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainMenuScreen from '../Page/Menu/MainMenuScreen';
-import SearchScreen from '../Page/Menu/SearchScreen';
-import ToolsScreen from '../Page/Menu/ToolsScreen';
 
 const Stack = createStackNavigator();
 
